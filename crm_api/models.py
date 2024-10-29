28a08453842bbd453ef8df05f99db9fe907fdd6e
alexisg22 <garciaciroalexis@gmail.com>
2024-10-29 07:39:09 -0500

    estuctura BD

--- crm_api/models.py
+++ crm_api/models.py
@@ -13,16 +13,25 @@ class Usuarios(models.Model):
     apellidos = models.CharField(max_length=30)
     email = models.CharField(max_length=50)
     telefono = models.CharField(max_length=10)
-    direccion = models.TextField(max_length=150)
     rol = models.ForeignKey(Roles, on_delete=models.CASCADE)
-    
     
     def __str__(self):
         return f"{self.nit} - {self.nombres} {self.apellidos}"
+    
+class Chat(models.Model):
+    usuario = models.ForeignKey(Usuarios, on_delete=models.CASCADE)
+    mensaje = models.TextField(max_length=500)
+    fecha = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.usuario}: '{self.mensaje}'"
+    
 
 class Campañas(models.Model):
     nombre = models.CharField(max_length=50)
     descripcion = models.TextField(max_length=500)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    campos_opcionales = models.JSONField(default=dict, blank=True)
     
     def __str__(self):
         return self.nombre
@@ -34,13 +43,78 @@ class CampañasUsuarios(models.Model):
     
     def __str__(self):
         return f"usuario: {self.usuarios_id} -  campaña: {self.campañas_id}"
+    
+class Tipo_identificacion(models.Model):
+    nombre = models.CharField(max_length=20)
+    
+    def __str__(self):
+        return self.nombre
+    
+class Pais(models.Model):
+    nombre = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.nombre
+    
+    
+class Departamento(models.Model):
+    nombre = models.CharField(max_length=50)
+    pais = models.ForeignKey(Pais, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.nombre
+    
+class Ciudad(models.Model):
+    nombre = models.CharField(max_length=50)
+    departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.nombre
+    
+class Telefono(models.Model):
+    numero = models.CharField(max_length=10, primary_key=True)
+    tipo = models.CharField(max_length=30)
+    tipo_celular = models.CharField(max_length=30)
+    indicativo = models.CharField(max_length=5)
+    extension = models.CharField(max_length=5)
+    ciudad = models.ForeignKey(Ciudad, on_delete=models.CASCADE)
+    departamento = models.CharField(max_length=50)
+    rating = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.numero
+    
+class Direccion(models.Model):
+    pais = models.ForeignKey(Pais, on_delete=models.CASCADE)
+    ciudad = models.ForeignKey(Ciudad, on_delete=models.CASCADE)
+    departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
+    barrio = models.CharField(max_length=50)
+    vereda = models.CharField(max_length=20, blank=True)
+    calle = models.CharField(max_length=15, blank=True)
+    carrera = models.CharField(max_length=10, blank=True)
+    complemento = models.CharField(max_length=70, blank=True)
+    
+    def __str__(self):
+        return f"{self.calle} - {self.carrera} - {self.complemento} "
+
+class Canales(models.Model):
+    telefonico = models.BooleanField(default=True)
+    visita = models.BooleanField(default=False)
+    whatsapp = models.BooleanField(default=False)
+    email = models.BooleanField(default=False)
+    sms = models.BooleanField(default=False)
+    
 
 class Clientes(models.Model):
     nit = models.CharField(max_length=15, primary_key=True)
-    telefono = models.CharField(max_length=10)
+    tipo_id = models.ForeignKey(Tipo_identificacion, on_delete=models.CASCADE)
+    telefono = models.ForeignKey(Telefono, on_delete=models.CASCADE)
+    direccion = models.ForeignKey(Direccion, on_delete=models.CASCADE)
     nombres = models.CharField(max_length=40)
     apellidos = models.CharField(max_length=30)
     email = models.CharField(max_length=50)
+    campos_opcionales = models.JSONField(default=dict, blank=True)
+    canales_autorizados = models.ForeignKey(Canales, on_delete=models.CASCADE)
     
     def __str__(self):
         return f"{self.nit} - {self.telefono} - {self.nombres}"
@@ -48,6 +122,8 @@ class Clientes(models.Model):
 class Codeudores(models.Model):
     nit = models.CharField(max_length=15, primary_key=True)
     nombre = models.CharField(max_length=30)
+    telefono = models.ForeignKey(Telefono, on_delete=models.CASCADE)
+    direccion = models.ForeignKey(Direccion, on_delete=models.CASCADE)
     cliente = models.ForeignKey(Clientes, on_delete=models.CASCADE)
     
     def __str__(self):
@@ -71,23 +147,38 @@ class Obligaciones(models.Model):
     codigo = models.CharField(max_length=50, primary_key=True)
     campaña = models.ForeignKey(Campañas, on_delete=models.CASCADE) 
     cliente = models.ForeignKey(Clientes, on_delete=models.CASCADE)
+    fecha_obligacion = models.DateField()
+    fecha_vencimiento_cuota = models.DateField()
+    valor_capital = models.FloatField()
+    valor_mora = models.FloatField()
+    campos_opcionales = models.JSONField(default=dict, blank=True)
+    
     
     def __str__(self):
         return f"{self.campaña} - {self.cliente}"
+    
+class Acuerdo_Pago(models.Model):
+    valor_cuota = models.FloatField()
+    fecha_pago = models.DateField()
+    codigo_obligacion = models.ForeignKey(Obligaciones, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"obligacion: {self.codigo_obligacion} - valor: {self.valor_cuota} - fecha: {self.fecha_pago}"
 
 class Pagos(models.Model):
+    obligacion = models.ForeignKey(Obligaciones, on_delete=models.CASCADE)
     valor = models.IntegerField(default=0)
     fecha = models.DateField()
-    obligacion = models.ForeignKey(Obligaciones, on_delete=models.CASCADE)
     cliente = models.ForeignKey(Clientes, on_delete=models.CASCADE)
+    campos_opcionales = models.JSONField(default=dict, blank=True)
     
     def __str__(self):
         return f"{self.cliente} - {self.valor} - {self.fecha}"
 
 class ResultadosGestion(models.Model):
     nombre = models.CharField(max_length=60)
-    descripcion = models.TextField(max_length=200)
-    efectividad = models.BooleanField(default=False)
+    descripcion = models.TextField(max_length=200, blank=True)
+    efectividad = models.BooleanField(default=False, blank=True)
     estado = models.BooleanField(default=False)
     
     def __str__(self):
@@ -101,4 +192,4 @@ class Gestiones(models.Model):
     comentarios = models.TextField(max_length=200)
     
     def __str__(self):
-        return f"{self.usuario} - {self.cliente} - {self.resultado} - {self.fecha}"
+        return f"Usuario: {self.usuario}, Cliente: {self.cliente}, Resultado: {self.resultado}, Fecha: {self.fecha}"
--- crm_api/serializers.py
+++ crm_api/serializers.py
@@ -7,9 +7,20 @@ class RolesSerializer(serializers.ModelSerializer):
         fields = '__all__'
         
 class UsuariosSerializer(serializers.ModelSerializer):
+    nit = serializers.CharField()
+    nombres = serializers.CharField()
+    apellidos = serializers.CharField()
+    email = serializers.EmailField()
+    telefono = serializers.CharField()
+    direccion = serializers.CharField()
+    rol = serializers.SerializerMethodField()
+    
     class Meta:
         model = Usuarios
-        fields = '__all__'
+        fields = ['nit', 'nombres', 'apellidos', 'email', 'telefono', 'direccion', 'rol']
+        
+    def get_rol(self, obj):
+        return obj.rol.nombre
         
 class CampañasSerializer(serializers.ModelSerializer):
     class Meta:
@@ -32,9 +43,18 @@ class ReferenciasSerializer(serializers.ModelSerializer):
         fields = '__all__'
         
 class ObligacionesSerializer(serializers.ModelSerializer):
+    codigo = serializers.CharField()
+    campaña = serializers.SerializerMethodField()
+    cliente = serializers.SerializerMethodField()
     class Meta:
         model = Obligaciones
-        fields = '__all__'
+        fields = ['codigo', 'campaña', 'cliente', 'campos_opcionales']
+        
+    def get_campaña(self, obj):
+        return obj.campaña.nombre
+    
+    def get_cliente(self, obj):
+        return f"{obj.cliente.nombres} {obj.cliente.apellidos}"
         
 class PagosSerializer(serializers.ModelSerializer):
     class Meta:
@@ -47,17 +67,25 @@ class ResultadosGestionSerializer(serializers.ModelSerializer):
         fields = '__all__'
         
 class GestionesSerializer(serializers.ModelSerializer):
-    usuario = serializers.PrimaryKeyRelatedField(read_only=True)
-    cliente = serializers.PrimaryKeyRelatedField(read_only=True)
+    usuario = serializers.SerializerMethodField()
+    cliente = serializers.SerializerMethodField()
     resultado = serializers.CharField(source='resultado.nombre', read_only=True)
+    fecha = serializers.DateTimeField()
     comentarios = serializers.CharField(read_only=True)
     
     class Meta:
         model = Gestiones
-        fields = '__all__'
+        fields = ['usuario', 'cliente', 'resultado', 'fecha', 'comentarios']
         
     def get_usuario(self, obj):
-        return obj.usuario.id
+        return f"{obj.usuario.nombres} {obj.usuario.apellidos}"
     
     def get_cliente(self, obj):
-        return obj.cliente.id
+        return f"{obj.cliente.nombres} {obj.cliente.apellidos}"
+    
+    # Formatear la fecha sin segundos ni milisegundos
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        if instance.fecha:
+            representation['fecha'] = instance.fecha.strftime('%d-%m-%y %H:%M')
+        return representation
