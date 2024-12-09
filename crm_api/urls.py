9c304375edbc4137d27d24cc6a3e1db231a62a42
JulianG <juliangiral12@gmail.com>
2024-12-09 10:21:13 -0500

    se cambian modelos para subir obligaciones 9-12 10:21AM

--- crm_api/urls.py
+++ crm_api/urls.py
@@ -9,6 +9,7 @@ from .pagos.ActualizarAcuerdoPagos import ActualizarAcuerdosPagoView
 from .pagos.EjecutarPagos import EjecutarPagos
 from .pagos.PagosMasivos import PagosMasivos
 from .views import *
+from .obligaciones.crearCliente import ClientesViewSet
 
 router = DefaultRouter()
 
@@ -16,21 +17,21 @@ router.register(r'roles', RolesViewSet, basename='Roles')
 router.register(r'campanas-api', CampañasViewSet, basename='Campañas')
 router.register(r'clientes', ClientesViewSet, basename='Clientes')
 router.register(r'telefono_cliente', Telefono_clienteViewSet, basename='Telefono cliente')
-router.register(r'direccion_cliente', Direccion_clienteViewSet, basename='Dirección cliente')
+# router.register(r'direccion_cliente', Direccion_clienteViewSet, basename='Dirección cliente')
 router.register(r'referencias', ReferenciasViewSet, basename='Referencias')
+router.register(r'telefono_referencias', Telefono_referenciaViewSet, basename='Telefono referencia')
+# router.register(r'direccion_referencias', Direccion_referenciaViewSet, basename='Direccion referencias')
 router.register(r'obligaciones', ObligacionesViewSet, basename='Obligaciones')
 router.register(r'pagos', PagosViewSet, basename='Pagos')
 router.register(r'resultadosGestion', ResultadosGestionViewSet, basename='ResultadosGestion')
 router.register(r'gestiones', GestionesViewSet, basename='Gestiones')
 router.register(r'chat', ChatViewSet, basename='Chat')
-router.register(r'tipo_identificacion', Tipo_identificacionViewSet, basename='Tipo de identificacion')
-router.register(r'pais', PaisViewSet, basename='País')
-router.register(r'departamento', DepartamentoViewSet, basename='Departamento')
-router.register(r'ciudad', CiudadViewSet, basename='Ciudad')
+# router.register(r'departamento', DepartamentoViewSet, basename='Departamento')
+# router.register(r'ciudad', CiudadViewSet, basename='Ciudad')
 router.register(r'codeudores', CodeudoresViewSet, basename='Codeudores')
 router.register(r'telefono_codeudor', Telefono_codeudorViewSet, basename='Telefono codeudor')
-router.register(r'direccion_codeudor', Direccion_codeudorViewSet, basename='Dirección codeudor')
-router.register(r'canales', CanalesViewSet, basename='Canales')
+# router.register(r'direccion_codeudor', Direccion_codeudorViewSet, basename='Dirección codeudor')
+# router.register(r'canales', CanalesViewSet, basename='Canales')
 router.register(r'acuerdo_pago', Acuerdo_pagoViewSet, basename='Acuerdos de Pago')
 router.register(r'CampanasUsuario', CampañaUsuarioViewSet, basename='campanasUsuario') 
 router.register(r'CustomUser', CustomUserViewSet, basename='custom-user') 
@@ -59,6 +60,7 @@ urlpatterns = [
     path('campanas/interacciones/', InteraccionCampañasView.as_view(), name='get_interacciones_por_fecha'),
     path('resultados_gestion/campaña/', ResultadosGestionView.as_view(), name='resultados_gestion_por_campaña'),
     path('campanas-por-usuario/', CampañasPorUsuario.as_view(), name='campanas-por-usuario'),  
-    path('borrarCampañas/', CampañaUsuarioDeleteView.as_view(), name='borrarCampañas'),  
+    path('borrarCampañas/', CampañaUsuarioDeleteView.as_view(), name='borrarCampañas'), 
+    path('cargarObligaciones/', CargarObligacionesViewSet.as_view(), name='cargar-obligaciones'), 
 
 ]
--- crm_api/views.py
+++ crm_api/views.py
@@ -4,7 +4,12 @@ from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.filters import OrderingFilter
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+import pandas as pd
 
+from  .obligaciones.cargarObligaciones import cargarObligaciones
 from .filters import *
 from .models import *
 from .models import CampañasUsuarios
@@ -36,15 +41,33 @@ class CampañaUsuarioViewSet(viewsets.ModelViewSet):
     filter_backends = (DjangoFilterBackend, OrderingFilter)
     filterset_class = CampañaUsuarioFilter
 
-class ClientesViewSet(viewsets.ModelViewSet):
-    queryset = Clientes.objects.all()
-    serializer_class = ClientesSerializer
+# class ClientesViewSet(viewsets.ModelViewSet):
+#     queryset = Clientes.objects.all()
+#     serializer_class = ClientesSerializer
 
-    filter_backends = (DjangoFilterBackend, OrderingFilter)
-    filterset_class = ClientesFilter
+#     filter_backends = (DjangoFilterBackend, OrderingFilter)
+#     filterset_class = ClientesFilter
 
-    # authentication_classes = [TokenAuthentication]
-    # permission_classes = [IsAuthenticated]
+#     # authentication_classes = [TokenAuthentication]
+#     # permission_classes = [IsAuthenticated]
+
+#     def crear_cliente_si_no_existe(self, datos_cliente):
+#         try:
+#             cliente, creado = Clientes.objects.get_or_create(
+#                 documento=datos_cliente['Documento cliente'],
+#                 defaults={
+#                     # Aquí agregas los campos para nuevos clientes
+#                     'nit': datos_cliente.get('Documento cliente'),
+#                     'nombres': datos_cliente.get('Nombre', ''),
+#                     'email': datos_cliente.get('Apellido', ''),
+#                     # Agrega más campos según tu modelo
+#                 }
+#             )
+#             return cliente
+#         except Exception as e:
+#             # Manejo de errores si falla la creación
+#             print(f"Error creando cliente: {e}")
+#             return None
 
 
 class CodeudoresViewSet(viewsets.ModelViewSet):
@@ -77,9 +100,86 @@ class ObligacionesViewSet(viewsets.ModelViewSet):
     filter_backends = (DjangoFilterBackend, OrderingFilter)
     filterset_class = ObligacionesFilter
 
-    # authentication_classes = [TokenAuthentication]
-    # permission_classes = [IsAuthenticated]
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+    # -----------------------------------------------------------------------------------------------------
+class CargarObligacionesViewSet(APIView):
+    def post(self, request, *args, **kwargs):
+        archivo = request.FILES.get('archivo')
+        id_campaña = request.data.get('id_campaign')
+        print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
 
+        print(id_campaña)
+
+        print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+        return cargarObligaciones( id_campaña, archivo)
+        # if not archivo:
+        #     return Response({'error': 'No se ha proporcionado un archivo'}, 
+        #                     status=status.HTTP_400_BAD_REQUEST)
+        # try:
+        #     df = pd.read_excel(archivo)
+        #     print (df)
+
+        #     # Columnas requeridas
+        #     columnas_requeridas = [
+        #         # 'Codigo obligacion',
+        #         'Codigo campaña',
+        #         'Documento cliente',
+        #         'Valor obligacion',
+        #         'Fecha obligacion',
+        #         'Fecha vencimiento',
+        #         'Valor mora' 
+        #     ]
+
+        #     if not all(col in df.columns for col in columnas_requeridas):
+        #             return Response({'error': 'Faltan columnas requeridas en el archivo'}, status=status.HTTP_400_BAD_REQUEST)
+        #     if('Codigo obligacion' in df.columns):
+        #         for _, fila in df.iterrows():
+    
+        #             obligacion = Obligaciones(
+        #                 codigo_obligacion=fila['Codigo obligacion'],
+        #                 campaña_id=fila['Codigo campaña'],
+        #                 cliente_id=fila['Documento cliente'],
+        #                 fecha_obligacion=fila['Fecha obligacion'],
+        #                 fecha_vencimiento_cuota=fila['Fecha vencimiento'],
+        #                 valor_capital=fila['Valor obligacion'],
+        #                 valor_mora=fila['Valor mora']
+        #             )
+
+        #             columnas_adicionales = {
+        #                 col: fila[col]
+        #                 for col in df.columns if col not in columnas_requeridas
+        #             }
+        #             obligacion.campos_opcionales = columnas_adicionales
+        #             obligacion.save()
+        #     else: # si no esta el codigo de la obligacion lo deja vacion
+        #         for _, fila in df.iterrows():
+    
+        #             obligacion = Obligaciones(
+        #                 codigo_obligacion= None,
+        #                 campaña_id=fila['Codigo campaña'],
+        #                 cliente_id=fila['Documento cliente'],
+        #                 fecha_obligacion=fila['Fecha obligacion'],
+        #                 fecha_vencimiento_cuota=fila['Fecha vencimiento'],
+        #                 valor_capital=fila['Valor obligacion'],
+        #                 valor_mora=fila['Valor mora']
+        #             )
+
+        #             columnas_adicionales = {
+        #                 col: fila[col]
+        #                 for col in df.columns if col not in columnas_requeridas
+        #             }
+        #             obligacion.campos_opcionales = columnas_adicionales
+        #             obligacion.save()
+
+        #     return Response({'mensaje': 'Obligaciones guardadas exitosamente'}, status=status.HTTP_201_CREATED)
+        # except Exception as e:
+        #     # import traceback
+        #     # error_trace = traceback.format_exc()
+        #     # print(error_trace)  # Rastreo completo del error en logs
+        #     return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    # -----------------------------------------------------------------------------------------------------
 
 class PagosViewSet(viewsets.ModelViewSet):
     queryset = Pagos.objects.all()
@@ -122,30 +222,16 @@ class ChatViewSet(viewsets.ModelViewSet):
     # authentication_classes = [TokenAuthentication]
     # permission_classes = [IsAuthenticated]
     
-class Tipo_identificacionViewSet(viewsets.ModelViewSet):
-    queryset = Tipo_identificacion.objects.all()
-    serializer_class = Tipo_identificacionSerializer
+# class DepartamentoViewSet(viewsets.ModelViewSet):
+#     queryset = Departamento.objects.all()
+#     serializer_class = DepartamentoSerializer
 
-    # authentication_classes = [TokenAuthentication]
-    # permission_classes = [IsAuthenticated]
+#     # authentication_classes = [TokenAuthentication]
+#     # permission_classes = [IsAuthenticated]
     
-class PaisViewSet(viewsets.ModelViewSet):
-    queryset = Pais.objects.all()
-    serializer_class = PaisSerializer
-
-    # authentication_classes = [TokenAuthentication]
-    # permission_classes = [IsAuthenticated]
-    
-class DepartamentoViewSet(viewsets.ModelViewSet):
-    queryset = Departamento.objects.all()
-    serializer_class = DepartamentoSerializer
-
-    # authentication_classes = [TokenAuthentication]
-    # permission_classes = [IsAuthenticated]
-    
-class CiudadViewSet(viewsets.ModelViewSet):
-    queryset = Ciudad.objects.all()
-    serializer_class = CiudadSerializer
+# class CiudadViewSet(viewsets.ModelViewSet):
+#     queryset = Ciudad.objects.all()
+#     serializer_class = CiudadSerializer
 
     # authentication_classes = [TokenAuthentication]
     # permission_classes = [IsAuthenticated]
@@ -163,24 +249,37 @@ class Telefono_codeudorViewSet(viewsets.ModelViewSet):
 
     # authentication_classes = [TokenAuthentication]
     # permission_classes = [IsAuthenticated]
-    
-class Direccion_clienteViewSet(viewsets.ModelViewSet):
-    queryset = Direccion_cliente.objects.all()
-    serializer_class = Direccion_clienteSerializer
+
+class Telefono_referenciaViewSet(viewsets.ModelViewSet):
+    queryset = Telefono_referencia.objects.all()
+    serializer_class = Telefono_referenciaSerializer
 
     # authentication_classes = [TokenAuthentication]
     # permission_classes = [IsAuthenticated]
     
-class Direccion_codeudorViewSet(viewsets.ModelViewSet):
-    queryset = Direccion_codeudor.objects.all()
-    serializer_class = Direccion_codeudorSerializer
+# class Direccion_clienteViewSet(viewsets.ModelViewSet):
+#     queryset = Direccion_cliente.objects.all()
+#     serializer_class = Direccion_clienteSerializer
 
     # authentication_classes = [TokenAuthentication]
     # permission_classes = [IsAuthenticated]
     
-class CanalesViewSet(viewsets.ModelViewSet):
-    queryset = Canales.objects.all()
-    serializer_class = CanalesSerializer
+# class Direccion_codeudorViewSet(viewsets.ModelViewSet):
+#     queryset = Direccion_codeudor.objects.all()
+#     serializer_class = Direccion_codeudorSerializer
+
+#     # authentication_classes = [TokenAuthentication]
+#     # permission_classes = [IsAuthenticated]
+    
+# class Direccion_referenciaViewSet(viewsets.ModelViewSet):
+#     queryset = Direccion_referencia.objects.all()
+#     serializer_class = Direccion_referenciaSerializer
+
+    # authentication_classes = [TokenAuthentication]
+    # permission_classes = [IsAuthenticated]
+# class CanalesViewSet(viewsets.ModelViewSet):
+#     queryset = Canales.objects.all()
+#     serializer_class = CanalesSerializer
 
     # authentication_classes = [TokenAuthentication]
     # permission_classes = [IsAuthenticated]
