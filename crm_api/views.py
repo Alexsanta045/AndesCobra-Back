e66ef9f65ad7e7d2edf0aa093f9153c051aa75b9
JeffersonMontoya <chechemontoya1997@gmail.com>
2024-12-27 07:52:35 -0500

    cambios 27 de diciembre cheche

--- crm_api/views.py
+++ crm_api/views.py
@@ -24,8 +24,8 @@ from .serializers.historialGestiones import HistorialGestionesSerializer
 class RolesViewSet(viewsets.ModelViewSet):
     queryset = Roles.objects.all()
     serializer_class = RolesSerializer
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
+    # authentication_classes = [TokenAuthentication]
+    # permission_classes = [IsAuthenticated]
 
 
 class CampañasViewSet(viewsets.ModelViewSet):
